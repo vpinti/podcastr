245c2fe480dfd669375e759ca77ea123041a3752
vpinti <vittorio.pinti@gmail.com>
2024-06-05 22:19:21 +0200

    feat: right sidebar

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -5,10 +5,13 @@ import Link from "next/link";
 import { sidebarLinks } from "@/constants";
 import { usePathname, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
+import { SignedIn, SignedOut, useClerk } from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
 const LeftSidebar = () => {
     const pathname = usePathname();
     const router = useRouter();
+    const { signOut } = useClerk();
 
     return (
         <section className="left_sidebar">
@@ -55,6 +58,26 @@ const LeftSidebar = () => {
                     );
                 })}
             </nav>
+            <SignedOut>
+                <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
+                    <Button
+                        asChild
+                        className="text-16 w-full bg-orange-1 font-extrabold"
+                    >
+                        <Link href="/sign-in">Sign In</Link>
+                    </Button>
+                </div>
+            </SignedOut>
+            <SignedIn>
+                <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
+                    <Button
+                        className="text-16 w-full bg-orange-1 font-extrabold"
+                        onClick={() => signOut(() => router.push("/"))}
+                    >
+                        Log Out
+                    </Button>
+                </div>
+            </SignedIn>
         </section>
     );
 };
