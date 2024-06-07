55d9fd02b00ef15fcc6201bb77d99de441fdf1a8
vpinti <vittorio.pinti@gmail.com>
2024-06-07 22:17:44 +0200

    feat: profile page

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -7,14 +7,20 @@ import { usePathname, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { SignedIn, SignedOut, useClerk } from "@clerk/nextjs";
 import { Button } from "./ui/button";
+import { useAudio } from "@/providers/AudioProvider";
 
 const LeftSidebar = () => {
     const pathname = usePathname();
     const router = useRouter();
     const { signOut } = useClerk();
+    const { audio } = useAudio();
 
     return (
-        <section className="left_sidebar">
+        <section
+            className={cn("left_sidebar h-[calc(100vh-5px)]", {
+                "h-[calc(100vh-140px)]": audio?.audioUrl,
+            })}
+        >
             <nav className="flex flex-col gap-6">
                 <Link
                     href="/"
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -9,16 +9,23 @@ import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useRouter } from "next/navigation";
 import LoaderSpinner from "./LoaderSpinner";
+import { useAudio } from "@/providers/AudioProvider";
+import { cn } from "@/lib/utils";
 
 const RightSidebar = () => {
     const { user } = useUser();
     const topPodcasters = useQuery(api.users.getTopUserByPodcastCount);
     const router = useRouter();
+    const { audio } = useAudio();
 
     if (!topPodcasters) return <LoaderSpinner />;
 
     return (
-        <section className="right_sidebar text-white-1">
+        <section
+            className={cn("right_sidebar h-[calc(100vh-5px)]", {
+                "h-[calc(100vh-140px)]": audio?.audioUrl,
+            })}
+        >
             <SignedIn>
                 <Link
                     href={`/profile/${user?.id}`}
